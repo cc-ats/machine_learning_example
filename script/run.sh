c77df420f8ae435bc711917a6b63bcef498a6714
yangjunjie <yangjunjie0320@ou.edu>
2020-12-14 11:23:26 -0600

    update

--- script/run.sh
+++ script/run.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=normal
+#SBATCH --exclusive
+#SBATCH --ntasks=20
+#SBATCH --ntasks-per-node=20
+#SBATCH --output=%j.out
+#SBATCH --error=%j.err
+#SBATCH --time=20:00:00
+#SBATCH --job-name=test
+
 module purge;
 module load deepmd-kit/1.2.1-foss-2020a;
 export OMP_NUM_THREADS=20
@@ -14,6 +24,9 @@ python script/build_system.py . 4;
 cd train;
 cp ../src/inp.json     .;
 cp ../src/train_cpu.sh .;
-sbatch train_cpu.sh;
 
+dp train     inp.json
+dp freeze -o model.pb
+
+cd ..;
 python script/test_model.py .;
